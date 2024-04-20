63cf6f47f7d0ba7b91258c70e2ec2a75a07f72ce
Mariya Zaharieva <mariqtrunkova@gmail.com>
2024-04-20 15:46:28 +0300

    Add Hero component

---
+++ src/app/components/Hero/Hero.tsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+    return (
+        <div className="">
+            <h1 className="w-auto text-center text-6xl font-bold font-font-sg text-custom-green y-4 px-6 my-10">Гъвкави решения за <br/>
+             сухопътен транспорт за <br/>
+             всеки клиент</h1>
+            <div className="flex justify-center items-center h-screen">
+                <img src='/hero-img.png' className="mx-auto w-7/12 h-full my-12" alt='hero img'/>
+            </div>
+        </div>
+    )
+
+}
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 export default function Home() {
   return (
     <main>
       <Header/>
+      <Hero/>
     </main>
   );
 }
