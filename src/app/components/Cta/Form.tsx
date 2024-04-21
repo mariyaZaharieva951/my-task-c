3df5836a6f3644d153623869b96f45b665d48a64
Mariya Zaharieva <mariqtrunkova@gmail.com>
2024-04-21 18:10:30 +0300

    Add logic for phone number, validation and messages for responses

--- src/app/components/Cta/Form.tsx
+++ src/app/components/Cta/Form.tsx
@@ -1,9 +1,48 @@
+'use client'
+import { Pontano_Sans } from 'next/font/google';
+import { useState} from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Form = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [message, setMessage] = useState('');
+
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if(!phoneNumber) {
+      phoneNumber
+      setMessage('Моля, въведете валиден телефонен номер');
+      return
+    }
+    (phoneNumber)
+      toast.success(message, {position: 'top-center',
+      autoClose: 5000});
+      setPhoneNumber('');
+
+
+  } 
+
+  const handleChange = (e: any) => {
+    
+    setPhoneNumber(e.target.value);
+    setMessage("Телефонният номер беше изпратен успешно!");
+    
+  };
+
     return (
-             <form className="mx-auto max-w-9xl mb-5">
+             <form className="mx-auto max-w-9xl mb-5" onSubmit={handleSubmit}>
                 <div className="relative flex justify-center pb-10">
                     <div className="bg-no-repeat bg-left">
-                    <input className="w-[13rem] rounded-l py-4 text-xs pl-8" placeholder="Мобилен телефон"></input>
+                    <input
+                     className="w-[13rem] rounded-l py-4 text-xs pl-8" 
+                     placeholder="Мобилен телефон"
+                      onChange={handleChange}
+                      value={phoneNumber}
+                     pattern='[0-9]{10}'
+                     title='Моля, въведете 10 цифрен телефонен номер'
+                     ></input>
                     <svg className="absolute top-5 transform -translate-y-1.5 h-6 w-4 pl-1.5 pb-1 ml-1 fill-current text-gray-400" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
     <path fill="none" stroke="currentColor" d="M10 12.221l8.343-6.628c.195-.154.39-.067.39.195V16.5c0 .216-.174.5-.5.5H1.5c-.276 0-.5-.218-.5-.5v-10c0-.262.214-.35.39-.195L10 12.221zm9.447-7.738A1.5 1.5 0 0 0 18.52 4H1.48a1.5 1.5 0 0 0-1.373 2.067l8.447 8A1.5 1.5 0 0 0 10 14a1.5 1.5 0 0 0 1.373-2.067l8.447-8zM1.28 5.309L10 12.779l8.72-6.47a.5.5 0 0 0-.561-.776L10 11.779l-7.159-5.24a.5.5 0 0 0-.56.77z"></path>
   </svg></div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,20 @@ import Solutions from "./components/Content/Solutions";
 import Advantages from "./components/Content/Advantages";
 import Cta from "./components/Cta/Cta";
 import Services from "./components/Services/Services";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
   return (
     <main>
+      <ToastContainer />
       <Header/>
       <Hero/>
       <Solutions/>
       <Advantages/>
       <Cta/>
       <Services/>
+      
     </main>
   );
 }
